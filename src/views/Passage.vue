8bea1c2d41566936fb07036734aed657cf64225c
Austin Baker <31426592+abaker95@users.noreply.github.com>
2019-04-17 21:51:30 -0600

    Update Passage.vue

--- src/views/Passage.vue
+++ src/views/Passage.vue
@@ -13,16 +13,65 @@
       <p>{{ passage.user.name }}</p>
       <p>{{ formatDate(passage.created) }}</p>
     </div>
-    <div v-if="passage.user.username === user.username">
-      <div>
-        <h3>Delete this Passage?</h3>
-        <p v-if="error" class="error">{{ error }}</p>
-        <button @click="deletePassage" class="pure-button">Delete</button>
+    <div v-if="user">
+      <div v-if="passage.user.username === user.username">
+        <div>
+          <h3>Delete or Edit this Passage?</h3>
+
+          <div v-if="show">
+            <button @click="deletePassage" class="pure-button">Delete</button>
+            <button @click="toggleEditView" class="pure-button">Edit</button>
+          </div>
+          <div v-else>
+            <form
+              @submit.prevent="editPassage"
+              class="form pure-form pure-form-aligned"
+            >
+              <div class="heading">
+                <h2>Change Passage or Speaker</h2>
+              </div>
+              <fieldset>
+                <div class="pure-control-group">
+                  <label for="newpassage">Change Passage</label>
+                  <input
+                    v-model="newpassage"
+                    type="text"
+                    placeholder="Passage"
+                  />
+                </div>
+                <div class="pure-control-group">
+                  <label for="newspeaker">Change Speaker</label>
+                  <input
+                    v-model="newspeaker"
+                    type="text"
+                    placeholder="Speaker"
+                  />
+                </div>
+                <p v-if="error" class="error">{{ error }}</p>
+                <div class="result" v-if="modified">
+                  Successfully updated the record in the database
+                  <p></p>
+                </div>
+
+                <button type="submit" class="pure-button">
+                  Submit
+                </button>
+                <button
+                  @click="toggleEditView"
+                  type="button"
+                  class="pure-button"
+                >
+                  Cancel
+                </button>
+              </fieldset>
+            </form>
+          </div>
+        </div>
       </div>
-    </div>
-    <div v-else>
-      <h3>Owners can login to remove</h3>
-      <router-link to="/login" class="pure-button">Login</router-link>
+      <div v-else>
+        <h3>Owners can login to remove</h3>
+        <router-link to="/login" class="pure-button">Login</router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -33,8 +82,11 @@ export default {
   name: "Passage",
   data() {
     return {
-      show: false,
-      error: null
+      show: true,
+      error: null,
+      newpassage: "",
+      newspeaker: "",
+      modified: false
     };
   },
   async created() {
@@ -42,7 +94,6 @@ export default {
     console.log("ID: " + passageId);
     await this.$store.dispatch("getUser");
     await this.$store.dispatch("getPassageById", passageId);
-    this.show = true;
   },
   computed: {
     user() {
@@ -58,6 +109,13 @@ export default {
         return moment(date).fromNow();
       else return moment(date).format("d MMMM YYYY");
     },
+    toggleEditView() {
+      if (this.show) {
+        this.show = false;
+      } else {
+        this.show = true;
+      }
+    },
     async deletePassage() {
       try {
         this.error = await this.$store.dispatch(
@@ -65,6 +123,17 @@ export default {
           this.passage._id
         );
         if (this.error === "") this.$router.push("manage");
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async editPassage() {
+      try {
+        this.error = await this.$store.dispatch("editPassageById", {
+          passage: this.newpassage,
+          speaker: this.newspeaker,
+          passageId: this.passage._id
+        });
       } catch (error) {
         console.log(error);
       }
